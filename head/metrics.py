527851ac025fd5f37994e4c1a41f0739e8c57fd5
Yan Sun <michael.yan720@gmail.com>
2022-03-05 22:13:03 -0500

    fix import

--- head/metrics.py
+++ head/metrics.py
@@ -3,7 +3,7 @@ from __future__ import division
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import Parameter
+from torch.nn import Parameter,Module
 import math
 
 
